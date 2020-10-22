70c32ad5a399378a876ccd87b77fcc468e07fab0
cvkm <cvkm0105@gmail.com>
2020-10-22 21:37:34 +0530

    hookTest

---
+++ Gprof_Test.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+void func4() 
+
+{
+
+    cout<<"Gprof Testing"<<endl;
+
+}
+
+void func3() 
+
+{
+
+    cout<<"Function 3"<<endl;
+
+}
+
+void func2()
+
+{
+    cout<<"Function 2"<<endl;
+    int i = 5000;
+    while(i>0){
+        func3();
+        i--;
+    }
+}
+
+void func1()
+
+{
+    cout<<"Function 1"<<endl;
+    int i=1000;
+    while(i>0){
+        func2();
+        i--;
+    }
+}
+
+
+int main() 
+{
+    func1();
+    func4();
+    return 0;
+}
